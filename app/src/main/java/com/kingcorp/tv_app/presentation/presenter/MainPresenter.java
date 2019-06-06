7267bbaa453d791b72b9a09c6a4ecda9b5f50ee0
Shakhzodbek Isomiddinov <shakhzodbek.isomiddinov.gmail.com>
2019-06-06 14:58:25 +0500

    Экран загрузки и Главный экран: инициализация

---
+++ app/src/main/java/com/kingcorp/tv_app/presentation/presenter/MainPresenter.java
@@ -0,0 +1,4 @@
+package com.kingcorp.tv_app.presentation.presenter;
+
+public class MainPresenter {
+}
---
+++ app/src/main/java/com/kingcorp/tv_app/presentation/view/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.kingcorp.tv_app.presentation.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
